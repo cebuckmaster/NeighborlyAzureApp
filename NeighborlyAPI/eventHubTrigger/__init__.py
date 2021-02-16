fd1d18d814fb5d9b124fe982e05bc0ff28bfa703
cebuckmaster <cebuckmaster@gmail.com>
2021-02-16 17:10:15 -0600

    Updates for Second Review

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -5,10 +5,10 @@ import azure.functions as func
 
 def main(event: func.EventGridEvent):
 
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
+#	logging.info('Function triggered to process a message: ', event.get_body())
+#    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
+#    logging.info('  SequenceNumber =', event.sequence_number)
+#    logging.info('  Offset =', event.offset)
 
     result = json.dumps({
         'id': event.id,
@@ -17,8 +17,7 @@ def main(event: func.EventGridEvent):
         'subject': event.subject,
         'event_type': event.event_type,
     })
-
-
+    
     logging.info('Python EventGrid trigger processed an event: %s', result)
 
 
